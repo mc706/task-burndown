4fa4f31b2400591bed35a703f53803d4220bcb84
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-30 17:40:00 -0400

    added rest urls

--- task_burndown/urls.py
+++ task_burndown/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
