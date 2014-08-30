7b4e4aaf3531e901f2ca578a9a4e864ab351c4ed
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-30 17:29:00 -0400

    added task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    """
+    Stores a single task.
+    account ties task with a :model:`auth.User`
+
+    """
+    account = models.ForeignKey(User)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    completed = models.BooleanField(default=False)
+
+    weight = models.IntegerField()
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_closed = models.DateTimeField(blank=True)
+
+    def __unicode__(self):
+        return self.title
+
