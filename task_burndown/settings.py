74bfdb71c8144113e1a0120794b9a4b75002a481
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-30 17:46:15 -0400

    added django rest settings

--- task_burndown/settings.py
+++ task_burndown/settings.py
@@ -71,6 +71,14 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
