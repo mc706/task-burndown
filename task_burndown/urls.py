77fed432a383318c49ba14fcf75237513c539345
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-30 17:42:45 -0400

    added user serializer

--- task_burndown/urls.py
+++ task_burndown/urls.py
@@ -1,8 +1,26 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = patterns('',
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^', include(router.urls)),
+    url(r'^admin/doc/$', include('django.contrib.admindocs.urls')),
+    url(r'^admin/$', include(admin.site.urls)),
+    url(r'^api-auth/$', include('rest_framework.urls', namespace='rest_framework'))
 )
