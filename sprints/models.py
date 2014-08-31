56fa814bfde4e04f5889a8d82c716482b9f3555e
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-30 20:01:12 -0400

    added sprints app

---
+++ sprints/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+from tasks.models import Task
+
+
+class Sprint(models.Model):
+    """
+    Model to have a sprint
+    account related to :model:`auth.User`
+    related to :model:`tasks.Task`
+
+    """
+    account = models.ForeignKey(User)
+    name = models.CharField(max_length=100, blank=True, help_text="Sprint Name")
+    date_start = models.DateField()
+    date_finish = models.DateField()
+    tasks = models.ManyToManyField(Task, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def get_sprint_total(self):
+        return sum([task.weight for task in self.tasks.all()])
+
+    def get_active_total(self):
+        return sum({task.weight for task in self.tasks.all() if not task.completed})
+
+    def get_burndown(self):
+        #TODO: Burndown chart calculation
+        pass
+
+    class Meta:
+        order = ('date_start',)
