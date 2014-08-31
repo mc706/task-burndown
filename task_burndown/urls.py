75b5bb46e5eb83d55f774d6669c59576afacc895
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-30 20:45:39 -0400

    added login and logout

--- task_burndown/urls.py
+++ task_burndown/urls.py
@@ -1,26 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
+from accounts.serializers import UserViewSet
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = patterns('',
-    url(r'^', include(router.urls)),
-    url(r'^admin/doc/$', include('django.contrib.admindocs.urls')),
-    url(r'^admin/$', include(admin.site.urls)),
-    url(r'^api-auth/$', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', 'task_burndown.api.api_root'),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', 'accounts.views.login_user', name='login'),
+    url(r'^logout/', 'accounts.views.logout_user', name='logout'),
+    url(r'^register/', 'accounts.views.register', name='register'),
+    url(r'^$', 'accounts.views.home', name='home'),
 )
