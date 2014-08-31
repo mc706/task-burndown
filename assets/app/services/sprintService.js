02dd64d0615691deabd39462c25643181e0a26f6
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-30 21:28:42 -0400

    basic services

--- assets/app/services/sprintService.js
+++ assets/app/services/sprintService.js
@@ -1,3 +1,69 @@
-/**
- * Created by Ryan.McDevitt on 8/30/14.
- */
+app.service('SprintService', function ($http, $q, $rootScope) {
+    "use strict";
+    return {
+        listSprints: function () {
+            var defer = $q.defer();
+            $http({
+                method: 'GET',
+                url: '/api/sprints/'
+            }).success(function (data, status, headers, config) {
+                defer.resolve(data);
+            }).error(function (data, status, headers, config) {
+                defer.reject(status);
+            });
+            return defer.promise;
+        },
+        getSprint: function (slug) {
+            var defer = $q.defer();
+            $http({
+                method: 'GET',
+                url: '/api/sprints/' + slug + '/'
+            }).success(function (data, status, headers, config) {
+                defer.resolve(data);
+            }).error(function (data, status, headers, config) {
+                defer.reject(status);
+            });
+            return defer.promise;
+        },
+        createSprint: function (data) {
+            var defer = $q.defer();
+            $http({
+                method: 'POST',
+                url: '/api/sprints/',
+                headers: {'Content-Type': 'application/json'},
+                data: $.param(data)
+            }).success(function (data, status, headers, config) {
+                defer.resolve(data);
+            }).error(function (data, status, headers, config) {
+                defer.reject(status);
+            });
+            return defer.promise;
+        },
+        updateSprint: function (slug, data) {
+            var defer = $q.defer();
+            $http({
+                method: 'PUT',
+                url: '/api/sprints/' + slug + '/',
+                headers: {'Content-Type': 'application/json'},
+                data: $.param(data)
+            }).success(function (data, status, headers, config) {
+                defer.resolve(data);
+            }).error(function (data, status, headers, config) {
+                defer.reject(status);
+            });
+            return defer.promise;
+        },
+        deleteSprint: function (slug) {
+            var defer = $q.defer();
+            $http({
+                method: 'DELETE',
+                url: '/api/sprints/' + slug + '/'
+            }).success(function (data, status, headers, config) {
+                defer.resolve(data);
+            }).error(function (data, status, headers, config) {
+                defer.reject(status);
+            });
+            return defer.promise;
+        }
+    };
+});
--- assets/app/services/taskService.js
+++ assets/app/services/taskService.js
@@ -1,3 +1,69 @@
-/**
- * Created by Ryan.McDevitt on 8/30/14.
- */
+app.service('TaskService', function ($http, $q, $rootScope) {
+    "use strict";
+    return {
+        listTasks: function () {
+            var defer = $q.defer();
+            $http({
+                method: 'GET',
+                url: '/api/tasks/'
+            }).success(function (data, status, headers, config) {
+                defer.resolve(data);
+            }).error(function (data, status, headers, config) {
+                defer.reject(status);
+            });
+            return defer.promise;
+        },
+        getTask: function (slug) {
+            var defer = $q.defer();
+            $http({
+                method: 'GET',
+                url: '/api/tasks/' + slug + '/'
+            }).success(function (data, status, headers, config) {
+                defer.resolve(data);
+            }).error(function (data, status, headers, config) {
+                defer.reject(status);
+            });
+            return defer.promise;
+        },
+        createTask: function (data) {
+            var defer = $q.defer();
+            $http({
+                method: 'POST',
+                url: '/api/tasks/',
+                headers: {'Content-Type': 'application/json'},
+                data: $.param(data)
+            }).success(function (data, status, headers, config) {
+                defer.resolve(data);
+            }).error(function (data, status, headers, config) {
+                defer.reject(status);
+            });
+            return defer.promise;
+        },
+        updateTask: function (slug, data) {
+            var defer = $q.defer();
+            $http({
+                method: 'PUT',
+                url: '/api/tasks/' + slug + '/',
+                headers: {'Content-Type': 'application/json'},
+                data: $.param(data)
+            }).success(function (data, status, headers, config) {
+                defer.resolve(data);
+            }).error(function (data, status, headers, config) {
+                defer.reject(status);
+            });
+            return defer.promise;
+        },
+        deleteTask: function (slug) {
+            var defer = $q.defer();
+            $http({
+                method: 'DELETE',
+                url: '/api/tasks/' + slug + '/'
+            }).success(function (data, status, headers, config) {
+                defer.resolve(data);
+            }).error(function (data, status, headers, config) {
+                defer.reject(status);
+            });
+            return defer.promise;
+        }
+    };
+});
