a99a46e0f75d3d472ed2fbd41b467e0868d3dae3
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-31 02:12:49 -0400

    updated debug statements

--- assets/app/controllers/homeController.js
+++ assets/app/controllers/homeController.js
@@ -1,5 +1,10 @@
-app.controller("HomeController", function ($scope, tasks, sprints) {
+app.controller("HomeController", function ($scope, $log, tasks, sprints) {
     'use strict';
+    $log.debug('Home Controller Loaded');
     $scope.tasks = tasks;
+    $log.debug('Tasks:', $scope.tasks);
     $scope.sprints = sprints;
+    $log.debug('Sprints:', $scope.sprints);
+
+
 });
