b064aca7f2c3cd06f7a7cbf7cb510e6c2015cae3
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-31 09:46:34 -0400

    readded DRF permissions

--- task_burndown/settings.py
+++ task_burndown/settings.py
@@ -76,9 +76,9 @@ DATABASES = {
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    #'DEFAULT_PERMISSION_CLASSES': [
-    #    'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    #]
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissions'
+    ]
 }
 
 # Internationalization
