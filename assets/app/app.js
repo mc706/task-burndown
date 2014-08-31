7c19e1da4ea8cb83e328402136564676fa919d9a
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-08-31 02:09:51 -0400

    turned of html5mode and reevalutaed debug password logic

--- assets/app/app.js
+++ assets/app/app.js
@@ -9,7 +9,8 @@ app.config(function ($logProvider) {
     "use strict";
     //Enables Debug when ?debug=1&password=*password*
     var password = "f48b9001e3972038d687a3dac8ebe8f9",
-        querystring = window.location.search.substring(1),
+        querystring = (window.location.search ? window.location.search.substring(1) :
+                window.location.hash.indexOf('?') !== -1 ? window.location.hash.split('?')[1] : ""),
         params = {};
     angular.forEach(querystring.split('&'), function (pair) {
         params[pair.split('=')[0]] = pair.split('=')[1];
@@ -31,12 +32,15 @@ app.config(function ($routeProvider, $locationProvider) {
             templateUrl: '/static/app/views/home.html',
             resolve: {
                 tasks: function (TaskService) {
-                    return TaskService.listTasks();
+                    //return TaskService.listTasks();
+                    return false;
                 },
                 sprints: function (SprintService) {
-                    return SprintService.listSprints();
+                    //return SprintService.listSprints();
+                    return false;
                 }
             }
-        });
-    $locationProvider.html5Mode(true);
+        }).when('/logout', function () {
+            window.location.replace();
+        }).otherwise({redirectTo: '/'});
 });
