c471125f52a11bd661e9cb189287f1860b0fba3d
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-09-05 09:31:38 -0400

    rewrite base permissions

--- task_burndown/permissions.py
+++ task_burndown/permissions.py
@@ -1,16 +1,11 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwner(permissions.BasePermission):
     """
-    Custom permission to only allow owners of an object to edit it.
+    Custom permission to only allow owners of an object to view or edit it.
+    Model instances are expected to include an `owner` attribute.
     """
 
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Write permissions are only allowed to the owner of the snippet.
         return obj.account == request.user
