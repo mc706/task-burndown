58ac0bd3229072a2d5ae5fa18704969ca81d4d8d
Ryan McDevitt <ryan_mcdevitt@cable.comcast.com>
2014-10-21 17:57:40 -0400

    added remote deploying

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,20 @@
-from fabric.api import lcd, local, warn_only
+from fabric.api import lcd, local, warn_only, cd, run
 
 
-def deploy():
-    #pulls new content and deploys it
+def local_deploy():
+    # pulls new content and deploys it
     local('git pull')
     local('python2.7 manage.py collectstatic --noinput')
     local('python2.7 manage.py migrate')
+
+
+def deploy():
+    with cd('/home/mc706/webapps/taskburn/taskburn'):
+        run('git pull')
+        run('python2.7 manage.py collectstatic --noinput')
+        run('python2.7 manage.py migrate')
+        #local('python2.7 manage.py test --noinput') #no tests written yet
+    with warn_only():
+        with cd('/home/mc706/webapps/taskburn/apache2/'):
+            run('bin/restart')
+
